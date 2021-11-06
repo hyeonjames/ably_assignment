b78e07055058d19483411efbf619c6052844eb26
hyeonjames <tmdgus0118@gmail.com>
2021-11-06 23:54:44 +0900

    Update docker settings

--- db/initdb.d/create_table.sql
+++ db/initdb.d/create_table.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE `ably`;
-CREATE TABLE `ably`.`users` (
+USE `db`
+CREATE TABLE `db`.`users` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `createdAt` datetime(6) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
@@ -9,4 +9,4 @@ CREATE TABLE `ably`.`users` (
   `name` varchar(255) DEFAULT NULL,
   `nickName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
